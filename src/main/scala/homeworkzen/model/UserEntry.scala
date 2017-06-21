2261cf429f574c23f2d0db01f5458bb67c157c27
anopse <@>
2017-06-21 13:13:23 +0200

    Added scaladoc to UserEntry

--- src/main/scala/homeworkzen/model/UserEntry.scala
+++ src/main/scala/homeworkzen/model/UserEntry.scala
@@ -2,4 +2,10 @@ package homeworkzen.model
 
 import java.util.UUID
 
+/** Case class containing basic information about user
+  *
+  * @param id       The UUID of the user (an user id must never change)
+  * @param username The username as used when authenticating
+  * @param secret   The hashed password to verify against
+  */
 case class UserEntry(id: UUID, username: String, secret: String)
