50b8fe0cecc4eae8107a9884e2c2affee1149817
anopse <@>
2017-06-25 22:21:11 +0200

    Changed akka persistence database to mysql

--- src/main/scala/homeworkzen/domain/utils/QueryHelper.scala
+++ src/main/scala/homeworkzen/domain/utils/QueryHelper.scala
@@ -2,14 +2,14 @@ package homeworkzen.domain.utils
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.persistence.query.journal.leveldb.scaladsl.LeveldbReadJournal
-import akka.persistence.query.{EventEnvelope2, PersistenceQuery}
+import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
+import akka.persistence.query.{EventEnvelope, PersistenceQuery}
 import akka.stream.scaladsl.Source
 
 object QueryHelper {
-  def currentEventsByTag(tag: String)(implicit actorSystem: ActorSystem): Source[EventEnvelope2, NotUsed] =
-    journal.currentEventsByTag(tag)
+  def currentEventsByTag(tag: String)(implicit actorSystem: ActorSystem): Source[EventEnvelope, NotUsed] =
+    journal.currentEventsByTag(tag, 0)
 
   private def journal(implicit actorSystem: ActorSystem) =
-    PersistenceQuery(actorSystem).readJournalFor[LeveldbReadJournal](LeveldbReadJournal.Identifier)
+    PersistenceQuery(actorSystem).readJournalFor[JdbcReadJournal](JdbcReadJournal.Identifier)
 }
